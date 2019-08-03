ae649625e02f01aa062222669e8ff6516c5c0377
Alberto Nidasio <albertonidasio@pop-os.localdomain>
2019-08-03 22:20:55 +0200

    Sistemate le cloud function, salvavano tutti gli orari in ciascun orario

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,61 +4,81 @@ import { ottieniOrariClassi, ottieniOrariAule, ottieniOrariProfessori } from 'pa
 admin.initializeApp()
 const firestore = admin.firestore()
 
-export const sincronizzaClassi = functions.pubsub.schedule('10 7 */1 * *').timeZone('Europe/Rome').onRun(async context => {
+export const sincronizzaClassi = functions.runWith({
+    memory: '1GB',
+    timeoutSeconds: 100
+}).region('europe-west2').pubsub.schedule('10 7 */1 * *').timeZone('Europe/Rome').onRun(async context => {
     try {
+        console.time('Classi')
+
         const orariClassi = await ottieniOrariClassi('2019')
 
-        return Promise.all(orariClassi.orari.map(async classe => firestore.collection('Classi').doc(classe.nome).set({
-            ...orariClassi.orari,
+        await Promise.all(orariClassi.orari.map(async classe => firestore.collection('Classi').doc(classe.nome).set({
+            ...classe,
             ultimoAggiornamento: admin.firestore.Timestamp.now()
-        })).concat(
-            firestore.collection('Classi').doc('Indici').set({
-                lista: orariClassi.lista,
-                ultimoAggiornamento: admin.firestore.Timestamp.now()                
-            })
-        )).then(() => {
-            console.log('salvate ' + orariClassi.lista.length + ' classi nel database con successo')
+        })))
+
+        console.timeEnd('Classi')
+
+        await firestore.collection('Classi').doc('Indici').set({
+            lista: orariClassi.lista,
+            ultimoAggiornamento: admin.firestore.Timestamp.now()                
         })
+        
+        console.log('salvate ' + orariClassi.lista.length + ' classi nel database con successo')
     } catch(err) {
         throw err
     }
 })
 
-export const sincronizzaAule = functions.pubsub.schedule('15 7 */1 * *').timeZone('Europe/Rome').onRun(async context => {
+export const sincronizzaAule = functions.runWith({
+    memory: '1GB',
+    timeoutSeconds: 100
+}).region('europe-west2').pubsub.schedule('15 7 */1 * *').timeZone('Europe/Rome').onRun(async context => {
     try {
+        console.time('Aule')
+
         const orariAule = await ottieniOrariAule('2019')
 
-        return Promise.all(orariAule.orari.map(async aula => firestore.collection('Aule').doc(aula.nome).set({
-            ...orariAule.orari,
+        await Promise.all(orariAule.orari.map(async aula => firestore.collection('Aule').doc(aula.nome).set({
+            ...aula,
             ultimoAggiornamento: admin.firestore.Timestamp.now()
-        })).concat(
-            firestore.collection('Aule').doc('Indici').set({
-                lista: orariAule.lista,
-                ultimoAggiornamento: admin.firestore.Timestamp.now()                
-            })
-        )).then(() => {
-            console.log('salvate ' + orariAule.lista.length + ' aule nel database con successo')
+        })))
+
+        console.timeEnd('Aule')
+
+        await firestore.collection('Aule').doc('Indici').set({
+            lista: orariAule.lista,
+            ultimoAggiornamento: admin.firestore.Timestamp.now()                
         })
+        
+        console.log('salvate ' + orariAule.lista.length + ' aule nel database con successo')
     } catch(err) {
         throw err
     }
 })
 
-export const sincronizzaProfessori = functions.pubsub.schedule('20 7 */1 * *').timeZone('Europe/Rome').onRun(async context => {
+export const sincronizzaProfessori = functions.runWith({
+    memory: '1GB',
+    timeoutSeconds: 100
+}).region('europe-west2').pubsub.schedule('20 7 */1 * *').timeZone('Europe/Rome').onRun(async context => {
     try {
+        console.time('Professori')
         const orariProfessori = await ottieniOrariProfessori()
 
-        return Promise.all(orariProfessori.orari.map(async professore => firestore.collection('Professori').doc(professore.nome).set({
-            ...orariProfessori.orari,
+        await Promise.all(orariProfessori.orari.map(async professore => firestore.collection('Professori').doc(professore.nome).set({
+            ...professore,
             ultimoAggiornamento: admin.firestore.Timestamp.now()
-        })).concat(
-            firestore.collection('Professori').doc('Indici').set({
-                lista: orariProfessori.lista,
-                ultimoAggiornamento: admin.firestore.Timestamp.now()                
-            })
-        )).then(() => {
-            console.log('salvati ' + orariProfessori.lista.length + ' professori nel database con successo')
+        })))
+
+        console.timeEnd('Professori')
+
+        await firestore.collection('Professori').doc('Indici').set({
+            lista: orariProfessori.lista,
+            ultimoAggiornamento: admin.firestore.Timestamp.now()                
         })
+        
+        console.log('salvati ' + orariProfessori.lista.length + ' professori nel database con successo')
     } catch(err) {
         throw err
     }
