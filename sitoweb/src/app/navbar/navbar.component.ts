0945df026a24161fe374f755cd0cbf623d0f59be
NidasioAlberto <nidasioalberto@gmail.com>
2019-08-24 20:49:04 +0200

    Aggiunto google analytics, modificato l'hint nella navbar e altro

--- sitoweb/src/app/navbar/navbar.component.ts
+++ sitoweb/src/app/navbar/navbar.component.ts
@@ -1,18 +1,35 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { take, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
   @Output() aggiornaValoreRicerca = new EventEmitter<string>();
+  valoreRicerca: string
 
   constructor(
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.pipe(take(2)).subscribe(queryParams => {
+      console.log(queryParams)
+      this.valoreRicerca = queryParams.valore
+    })
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      if (!event.url.includes('ricerca')) {
+        this.valoreRicerca = ''
+      }
+    })
+  }
 
   tornaAllaHome() {
     this.router.navigate(['/'])
