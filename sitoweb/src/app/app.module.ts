c64501cbc7a5e9b189cea784165d07d70ab52ca8
NidasioAlberto <nidasioalberto@gmail.com>
2019-11-11 13:20:28 +0100

    Include le librerie @angular/fire e @ngx-pwa/local-storage, aggiunti anche dei comandi npm

--- sitoweb/src/app/app.module.ts
+++ sitoweb/src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ServiceWorkerModule } from '@angular/service-worker'
+import { environment } from '../environments/environment'
+
+// Moduli firebase e storage
+import { StorageModule } from '@ngx-pwa/local-storage'
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+import { AngularFirePerformanceModule } from '@angular/fire/performance'
 
 @NgModule({
   declarations: [
@@ -15,7 +21,13 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+
+    // Moduli firebase e storage
+    StorageModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence(),
+    AngularFirePerformanceModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
