9406d9702d6f7b533061c506e04a796f923d06d3
NidasioAlberto <nidasioalberto@gmail.com>
2019-11-27 13:25:58 +0100

    Rimosso un commento

--- sitoweb/src/app/navbar/navbar.component.ts
+++ sitoweb/src/app/navbar/navbar.component.ts
@@ -4,7 +4,6 @@ import { Router, ActivatedRoute, NavigationEnd } from '@angular/router'
 import { take, filter } from 'rxjs/operators'
 import { StorageService } from '../core/storage.service'
 import { MatSnackBar } from '@angular/material/snack-bar'
-import undefined = require('firebase/empty-import')
 
 /**
  * Questo componente è responsibile della navbar visualizzata in ogni pagina.
@@ -157,8 +156,6 @@ export class NavbarComponent implements OnInit {
   }
 
   aggiornaValoriRicerca(valoreRicerca?: string, filtro?: string, event?: KeyboardEvent) {
-    console.log('Aggiorna valore ricerca', valoreRicerca, event)
-
     // Chiudo gli strumenti se viene premuto invio
     if(event !== undefined && event.key === 'Enter') this.stato = 'chiuso'
     else this.stato = 'aperto'
