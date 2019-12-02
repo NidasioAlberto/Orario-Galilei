96705bb121d600a1606236c226d65f9d683663e3
NidasioAlberto <nidasioalberto@gmail.com>
2019-12-02 15:18:08 +0100

    Aggiunto l'avviso della disponibilità di nuove versioni

--- sitoweb/src/app/app.component.ts
+++ sitoweb/src/app/app.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { SwUpdate } from '@angular/service-worker'
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  constructor(
+    private swUpdate: SwUpdate,
+    private snakBar: MatSnackBar,
+  ) { }
+
+  ngOnInit() {
+    // Notifico l'utente quando una nuova versione dell'app è disponibile
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        this.snakBar.open('Nuova versione disponibile', 'Aggiorna', { duration: 5000 }).onAction().subscribe(() => {
+          // L'utente vuole aggiornare la pagina, la ricarico
+          window.location.reload()
+        })
+      })
+    }
+  }
+}
